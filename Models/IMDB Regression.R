1445aae1a98db0a3dbd5004fc523f3c528c49a67
Saif Khaja <121301100+Saif-Khaja@users.noreply.github.com>
2022-12-26 19:54:06 +0100

    Add files via upload

---
+++ Models/IMDB Regression.R
@@ -0,0 +1,42 @@
+# Wed Nov 12 19:27:25 2022 ------------------------------
+library(tidyverse);library(rvest);library(broom)
+
+# Run the 'Webscraping IMDB and Boxoffice Worldwide scrips before you run this or else it wont work
+
+colnames(df)[2] <- "Movie"
+
+data <- inner_join(x = df, y = IMDB_Data, by=c("Movie", "Year"))
+
+data <- data %>%
+  select(c(-Rank,-Domestic:-`foreign_%`,-Director:-Stars,-Movie))
+
+data$Genre <- as_factor(data$Genre)
+
+# Multiple Linear Regression Model
+
+lm <- lm(Worldwide~., data = data)
+summary(lm)
+
+tlm <- lm %>% 
+  tidy();tlm
+
+augment(lm)
+glance(lm)
+
+tlm[,2:5] <- map_df(tlm[,2:5], round,4)
+
+
+# Beta estimates for significant predictors
+
+tlm %>% 
+  filter(p.value<0.05,
+         !str_detect(term,"(Intercept)")) %>% 
+  ggplot(aes(x=reorder(term,estimate), y=estimate, fill=term)) +
+  geom_col(show.legend = F) +
+  coord_flip() +
+  theme_minimal() +
+  scale_y_continuous(labels = scales::comma_format()) +
+  labs(x=NULL,
+       title="IMDB Regression Estimates on Worldwide Boxoffice") +
+  theme(axis.text = element_text(color="black"),
+        plot.title = element_text(face="bold"))
